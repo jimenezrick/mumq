ba66dbc17c5a8c6e31947bf030c606f0446b7a03
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-02-05 13:35:01 +0100

    Improve start.sh script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-erl -pa ebin -pa deps/*/ebin -boot start_sasl -run mumq
+erl -pa ebin -pa deps/*/ebin -boot start_sasl -config rel/files/app.config -args_file rel/files/vm.args -run mumq
