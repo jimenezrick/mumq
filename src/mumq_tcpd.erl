4765db20bc9249ed24fd7508fe43b95d2b0b90e0
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-02-26 15:29:25 +0100

    Fix SSL, now it works

--- src/mumq_tcpd.erl
+++ src/mumq_tcpd.erl
@@ -12,9 +12,23 @@
 -include("mumq.hrl").
 
 start_link(Name, Type, Port) ->
+    case Type of
+        ssl ->
+            case
+                {application:get_env(ssl_certfile),
+                 application:get_env(ssl_keyfile)}
+            of
+                {{ok, CertFile}, {ok, KeyFile}} ->
+                    SslOpts = [{certfile, CertFile}, {keyfile, KeyFile}];
+                _ ->
+                    SslOpts = []
+            end;
+        _ ->
+            SslOpts = []
+    end,
     gen_tcpd:start_link({local, Name}, ?MODULE, Name, Type, Port,
                         [{acceptors, ?TCP_ACCEPTORS},
-                         {socket_options, ?TCP_OPTS ++ [{reuseaddr, true}]}]).
+                         {socket_options, ?TCP_OPTS ++ [{reuseaddr, true}] ++ SslOpts}]).
 
 init(mumq_tcpd) ->
     process_flag(trap_exit, true),
